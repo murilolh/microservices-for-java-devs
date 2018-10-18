dd9a294d4ed754afbe6ecb97a33697abf183e1ea
Murilo Holanda <murilolh@gmail.com>
2018-10-18 15:39:20 -0300

    Dropwizard initial tests

--- hola-dropwizard/src/main/java/com/redhat/examples/dropwizard/HolaDropwizardApplication.java
+++ hola-dropwizard/src/main/java/com/redhat/examples/dropwizard/HolaDropwizardApplication.java
@@ -1,29 +1,35 @@
 package com.redhat.examples.dropwizard;
 
+import com.redhat.examples.dropwizard.resources.HolaRestResource;
+
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class HolaDropwizardApplication extends Application<HolaDropwizardConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new HolaDropwizardApplication().run(args);
-    }
+	public static void main(final String[] args) throws Exception {
+		new HolaDropwizardApplication().run(args);
+	}
 
-    @Override
-    public String getName() {
-        return "HolaDropwizard";
-    }
+	@Override
+	public String getName() {
+		return "HolaDropwizard";
+	}
 
-    @Override
-    public void initialize(final Bootstrap<HolaDropwizardConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+	@Override
+	public void initialize(final Bootstrap<HolaDropwizardConfiguration> bootstrap) {
+		// Enable variable substitution with environment variables
+		bootstrap.setConfigurationSourceProvider(new SubstitutingSourceProvider(
+				bootstrap.getConfigurationSourceProvider(), new EnvironmentVariableSubstitutor(false)));
+	}
 
-    @Override
-    public void run(final HolaDropwizardConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+	@Override
+	public void run(final HolaDropwizardConfiguration configuration, final Environment environment) {
+		environment.jersey().register(new HolaRestResource(configuration.getSayingFactory().getSaying()));
+
+	}
 
 }
--- hola-dropwizard/src/main/java/com/redhat/examples/dropwizard/HolaDropwizardConfiguration.java
+++ hola-dropwizard/src/main/java/com/redhat/examples/dropwizard/HolaDropwizardConfiguration.java
@@ -1,7 +1,20 @@
 package com.redhat.examples.dropwizard;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.redhat.examples.dropwizard.resources.HelloSayingFactory;
+
 import io.dropwizard.Configuration;
 
 public class HolaDropwizardConfiguration extends Configuration {
-    // TODO: implement service configuration
+	private HelloSayingFactory sayingFactory;
+
+	@JsonProperty("helloapp")
+	public HelloSayingFactory getSayingFactory() {
+		return sayingFactory;
+	}
+
+	@JsonProperty("helloapp")
+	public void setSayingFactory(HelloSayingFactory sayingFactory) {
+		this.sayingFactory = sayingFactory;
+	}
 }
